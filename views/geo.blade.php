17480bbe0315bf386f207826db18e17774413ca8
aprilia <93498647+aprilia94@users.noreply.github.com>
2022-01-10 08:22:55 +0700

    Add files via upload

---
+++ views/geo.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+@section('content')
+<div class="col-sm">
+<div align="center">
+<div class="card" style="width: 70rem;">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Republik_Indonesia.jpg" class="card-img-top" alt="gambar proklamasi">
+  <div class="card-body">
+  <h5 class="card-title">GEOGRAFIS INDONESIA</h5>
+    <p class="card-text">Dari Sabang di ujung Aceh sampai Merauke di tanah Papua, Indonesia terdiri dari berbagai suku bangsa, bahasa, dan agama. Berdasarkan rumpun bangsa (ras), Indonesia terdiri atas bangsa asli pribumi yakni Mongoloid Selatan/Austronesia dan Melanesia di mana bangsa Austronesia yang terbesar jumlahnya dan lebih banyak mendiami Indonesia bagian barat. Secara lebih spesifik, suku bangsa Jawa adalah suku bangsa terbesar dengan populasi mencapai 42% dari seluruh penduduk Indonesia.
+    Semboyan nasional Indonesia, "Bhinneka tunggal ika" ("Berbeda-beda namun tetap satu"), bermakna keberagaman sosial-budaya yang membentuk satu kesatuan/negara. Selain memiliki populasi penduduk yang padat dan wilayah yang luas, Indonesia memiliki wilayah alam yang mendukung tingkat keanekaragaman hayati terbesar kedua di dunia.
+Indonesia merupakan anggota dari Perserikatan bangsa-bangsa (PBB) dan satu-satunya anggota yang pernah keluar dari PBB, yaitu pada tanggal 7 Januari 1965, dan bergabung kembali pada tanggal 28 September 1966. Indonesia tetap dinyatakan sebagai anggota yang ke-60, keanggotaan yang sama sejak bergabungnya Indonesia pada tanggal 28 September 1950. Selain PBB, Indonesia juga negara anggota dari organisasi ASEAN, KAA, APEC, OKI, G-20 dan sebentar lagi akan menjadi anggota OECD.</p>
+  </div>
+</div>
+</div>
+</div>
+@endsection
