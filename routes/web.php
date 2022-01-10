fa0f325be3ca99233a74debf761c1bc0d19ca92f
aprilia <93498647+aprilia94@users.noreply.github.com>
2022-01-10 08:15:51 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('home.index');
+});
+
+Route::resource('home', 'HomeController');
+Route::resource('sejarah', 'SejarahController');
+Route::resource('geo', 'GeoController'); 
